bd46da4517aefb08ae2beccf0d14bad80a7de100
Mulriez <pesonarizqi17@gmail.com>
2022-11-09 14:04:06 +0700

    hook

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, {useRef} from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// function App(){
-//   let a = 20
-//   let b = 10
-//   return (
-//     <div>
-//       <h1>Hello World ke-{a}</h1>
-//       <h1>Hello World ke-{a+b}</h1>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-// function App() {
-//   let a = 20
-//   let b = 20
-//   return(
-//     <React.Fragment>
-//       <h1>SANGAT PAHAM UNTUK YG KE {a}</h1>
-//       <h1>SANGAT PAHAM UNTUK YG KE {a+b}</h1>
-//     </React.Fragment>
-//   );
-// }
-
-// export default App;
-
-import React from 'react';
-
-function App() {
-  return(
-    <React.Fragment>
-      <h1 className='bg-red-500'>my beat</h1>
-    </React.Fragment>
-  );
+export default function RefTutorial(){
+const inputRef = useRef(null);
+const onClick =() => {
+console.log(inputRef.current.value);
+inputRef.current.focus()
+inputRef.current.value = "SHIN"
+};
+return(
+  <div>
+    <h1>Black SUN</h1>
+    in
+  </div>
+)
 }
-
-export default App;

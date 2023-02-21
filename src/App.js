0222d44315b765da9541f20a26e42ea6c0521024
Mulriez <pesonarizqi17@gmail.com>
2023-02-21 14:24:59 +0700

    h

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dash from "./page/dash";
-import Detail from "./page/detailprod";
-import Fp from "./page/forgot";
-import History from "./page/history";
-import Keranjang from "./page/keranjang";
 import Login from "./page/login";
-import Register from "./page/register";
-import Reset from "./page/reset";
-import ProtectRoute from "./router/protectRoute";
+
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot" element={<Fp />} />
-        <Route path="/reset-password/:id/:token" element={<Reset />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectRoute>
-              <Dash />
-            </ProtectRoute>
-          }
-        />
-        <Route
-          path="/product/detail/:uuid"
-          element={
-            <ProtectRoute>
-              <Detail />
-            </ProtectRoute>
-          }
-        />
-        <Route
-          path="/keranjang"
-          element={
-            <ProtectRoute>
-              <Keranjang/>
-            </ProtectRoute>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectRoute>
-              <History/>
-            </ProtectRoute>
-          }
-        />
         <Route path="*" element={<Login />} />
       </Routes>
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store/index";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store/index";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <Router>
         <App />
       </Router>
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
